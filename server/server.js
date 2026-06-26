d60bc8a91959b9fdbbdf1867ea2ca8df9b733e6d
yx890809 <yx890809@gmail.com>
2026-06-26 23:58:07 +0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 const onlineUsers = new Map();
 const gameRooms = new Map();
 
